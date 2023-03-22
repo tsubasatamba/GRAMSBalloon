e601d75f526da59ec66af05c6571b5c5d76f8bb4
ShotaArai <shota.arai@phys.s.u-tokyo.ac.jp>
2023-03-22 21:38:43 +0900

    test on DAQIO.cc

--- examples/read_daq/Decode/decode_binary.cc
+++ examples/read_daq/Decode/decode_binary.cc
@@ -8,24 +8,36 @@ int main(int argc, char **argv)
     std::vector<short> data;
     char *FILENAME = argv[1];
     std::ifstream ifs(std::string(FILENAME), std::ios::binary);
-    char temp[2];
-    short _data;
-    for (int i = 0; i < 32; i++)
-    {
-        ifs.read(&temp[0], 2);
+    char temp[8146];
+    ifs.read(temp, sizeof(char) * 8146);
+    int index = 0;
+    while (index<64) {
+        short val = (static_cast<short>(temp[index])) + ((static_cast<short>(temp[index+1]))<<8);
+        //std::cout << (int)temp[index] << " " << (int)temp[index+1] << " " <<  val << std::endl;
+        index += 2;
     }
-    int i = 0;
-    while (!ifs.eof())
-    {
-        ifs.read(&temp[0], 2);
-        _data = static_cast<short>(temp[0]) + static_cast<short>(temp[1]) << 8;
 
-        std::cout << _data << " ";
-        if (i == 8)
-        {
+    const int num_events = 5;
+    const int num_channels = 4;
+    const int num_bins = 180;
+    for (int i=0; i<num_events; i++) {
+        const int ev_header_size = 5;
+        for (int j=0; j<ev_header_size; j++) {
+            index += 2;
+        }
+        for (int j=0; j<num_channels; j++) {
+            std::cout << "channel " << j << std::endl;
+            std::vector<short> data;
+            for (int k=0; k<num_bins; k++) {
+                short val = (static_cast<short>(temp[index])) + ((static_cast<short>(temp[index+1]))<<8);
+                data.push_back(val);
+                index += 2;
+            }
+            for (int k=0; k<num_bins; k++) {
+                std::cout << k << " " << data[k] << " ";
+            }
             std::cout << std::endl;
         }
-        i++;
     }
     
     return 0;
--- examples/read_daq/read_daq.rb
+++ examples/read_daq/read_daq.rb
@@ -10,9 +10,9 @@ class MyApp < ANL::ANLApp
             trig_device: 0,
             trig_channel: 0,
             trig_mode: 2,
-            trig_level: 1.2,
+            trig_level: 0.1,
             trig_position: 0.0,
-            time_window: 100.0, # us
+            time_window: 90.0, # us
             sample_frequency: 2.0, #MHz
             output_filename_base: "DAQ_output",
             num_events_per_file: 5
