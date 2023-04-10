a8a57be1b311f6cb5de5b4652e9224f1c485350e
Tsubasa Tamba <tamba@juno.phys.s.u-tokyo.ac.jp>
2023-04-10 18:49:19 +0900

    merged and fixed a new module ReceiveTelemetry.cc

--- source/modules/include/ReceiveTelemetry.hh
+++ source/modules/include/ReceiveTelemetry.hh
@@ -3,42 +3,40 @@
 
 #include <anlnext/BasicModule.hh>
 #include "SerialCommunication.hh"
-#include <queue>
 #include <TelemetryDefinition.hh>
 #include <thread>
 #include <chrono>
 
-namespace gramsballoon
-{
-    class ReceiveTelemetry : public anlnext::BasicModule
-    {
-        DEFINE_ANL_MODULE(ReceiveTelemetry, 1.0);
+namespace gramsballoon {
 
-    public:
-        ReceiveTelemetry();
-        virtual ~ReceiveTelemetry();
+class ReceiveTelemetry : public anlnext::BasicModule {
+  DEFINE_ANL_MODULE(ReceiveTelemetry, 1.0);
 
-    protected:
-        ReceiveTelemetry(const ReceiveTelemetry &r) = default;
+public:
+  ReceiveTelemetry();
+  virtual ~ReceiveTelemetry();
 
-    public:
-        anlnext::ANLStatus mod_define() override;
-        anlnext::ANLStatus mod_initialize() override;
-        anlnext::ANLStatus mod_analyze() override;
-        anlnext::ANLStatus mod_finalize() override;
+protected:
+  ReceiveTelemetry(const ReceiveTelemetry &r) = default;
+  
+public:
+  anlnext::ANLStatus mod_define() override;
+  anlnext::ANLStatus mod_initialize() override;
+  anlnext::ANLStatus mod_analyze() override;
+  anlnext::ANLStatus mod_finalize() override;
+  
+private:
+  std::vector<uint8_t> telemetry_;
+  std::shared_ptr<TelemetryDefinition> teldef_ = nullptr;
+  int maxTelemetry_ = 32000;
+  
+  // communication
+  std::shared_ptr<SerialCommunication> sc_ = nullptr;
+  speed_t baudrate_;
+  std::string serialPath_;
+  char openMode_ = O_RDWR;
+  bool startReading_ = false;
+};
 
-    private:
-        std::vector<uint8_t> telemetry_;
-        std::queue<uint8_t> que_;
-        std::shared_ptr<TelemetryDefinition> teldef_ = nullptr;
-        int maxTelemetry_ = 32000;
-
-        // communication
-        std::shared_ptr<SerialCommunication> sc_ = nullptr;
-        speed_t baudrate_;
-        std::string serialpath_;
-        char openMode_ = O_RDWR;
-        bool startReading_ = false;
-    };
 } // namespace gramsballoon
 #endif // ReceiveTelemetry_H
--- source/modules/src/ReceiveTelemetry.cc
+++ source/modules/src/ReceiveTelemetry.cc
@@ -1,84 +1,78 @@
 #include "ReceiveTelemetry.hh"
 using namespace anlnext;
 
-namespace gramsballoon
+namespace gramsballoon {
+
+ReceiveTelemetry::ReceiveTelemetry()
+  : maxTelemetry_(32000), baudrate_(B9600), openMode_(O_RDWR | O_NONBLOCK)
 {
-    ReceiveTelemetry::ReceiveTelemetry()
-        : baudrate_(B9600), openMode_(O_RDWR | O_NONBLOCK), maxTelemetry_(32000)
-    {
-        serialpath_ = "/dev/null";
-        for (int i = 0; i < 2; i++)
-        {
-            que_.push(0);
-        }
-        teldef_ = std::make_unique<TelemetryDefinition>();
-    }
+  serialPath_ = "/dev/null";
+  teldef_ = std::make_unique<TelemetryDefinition>();
+}
 
-    ReceiveTelemetry::~ReceiveTelemetry() = default;
-    ANLStatus ReceiveTelemetry::mod_define()
-    {
-        define_parameter("baudrate", &mod_class::baudrate_);
-        define_parameter("serial_path", &mod_class::serialpath_);
-        define_parameter("open_mode", &mod_class::openMode_);
+ReceiveTelemetry::~ReceiveTelemetry() = default;
 
-        return AS_OK;
-    }
+ANLStatus ReceiveTelemetry::mod_define()
+{
+  define_parameter("baudrate", &mod_class::baudrate_);
+  define_parameter("serial_path", &mod_class::serialPath_);
+  define_parameter("open_mode", &mod_class::openMode_);
+  
+  return AS_OK;
+}
 
-    ANLStatus ReceiveTelemetry::mod_initialize()
-    {
-        sc_ = std::make_unique<SerialCommunication>(serialpath_, baudrate_, openMode_);
-        sc_->initialize();
+ANLStatus ReceiveTelemetry::mod_initialize()
+{
+  sc_ = std::make_unique<SerialCommunication>(serialPath_, baudrate_, openMode_);
+  sc_->initialize();
+  
+  return AS_OK;
+}
 
-        return AS_OK;
-    }
-    ANLStatus ReceiveTelemetry::mod_analyze()
-    {
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+ANLStatus ReceiveTelemetry::mod_analyze()
+{
+  std::this_thread::sleep_for(std::chrono::seconds(1));
 
-        telemetry_.clear();
+  telemetry_.clear();
+  
+  fd_set fdset;
+  timeval timeout;
+  timeout.tv_sec = 10;
+  timeout.tv_usec = 0;
+  FD_ZERO(&fdset);
+  FD_SET(sc_->FD(), &fdset);
+  int rv = select(sc_->FD() + 1, &fdset, NULL, NULL, &timeout);
+  if (rv == -1) {
+    std::cerr << "Error in ReceiveTelemetry::mod_analyze: rv = -1" << std::endl;
+    return AS_ERROR;
+  }
 
-        fd_set fdset;
-        timeval timeout;
-        timeout.tv_sec = 10;
-        timeout.tv_usec = 0;
-        FD_ZERO(&fdset);
-        FD_SET(sc_->FD(), &fdset);
-        int rv = select(sc_->FD() + 1, &fdset, NULL, NULL, &timeout);
-        if (rv == -1)
-        {
-            std::cerr << "Error in ReceiveTelemetry::mod_analyze: rv = -1" << std::endl;
-            return AS_ERROR;
-        }
+  if (rv == 0) {
+    std::cout << "Time out" << std::endl;
+    return AS_OK;
+  }
 
-        if (rv == 0)
-        {
-            std::cout << "Time out" << std::endl;
-            return AS_OK;
-        }
+  std::vector<uint8_t> buffer(maxTelemetry_);
+  std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  const int status = sc_->sread(buffer, maxTelemetry_);
+  if (status == -1) {
+    std::cerr << "Read command failed in ReceiveTelemetry::mod_analyze: status = " << status << std::endl;
+    return AS_OK;
+  }
+  std::cout << "status: " << status << std::endl;
+  for (int i = 0; i < status; i++) {
+    telemetry_.push_back(buffer[i]);
+  }
+  #if 0
+  for (int i = 0; i < status;i++) {
+    std::cout << "telemetry[" <<i<< "] = "<<static_cast<int>(telemetry_[i]) << std::endl;
+  }
+  #endif
+}
 
-        std::vector<uint8_t> buffer(maxTelemetry_);
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
-        const int status = sc_->sread(buffer, maxTelemetry_);
-        if (status == -1)
-        {
-            std::cerr << "Read command failed in ReceiveTelemetry::mod_analyze: status = " << status << std::endl;
-            return AS_OK;
-        }
-        std::cout << "status: " << status << std::endl;
-        for (int i = 0; i < status; i++)
-        {
-            telemetry_.push_back(buffer[i]);
-        }
-#if 0
-        for (int i = 0; i < status;i++){
-            std::cout << "telemetry[" <<i<< "] = "<<static_cast<int>(telemetry_[i]) << std::endl;
-        }
-#endif
-    }
-
-    ANLStatus ReceiveTelemetry::mod_finalize()
-    {
-        return AS_OK;
-    }
+ANLStatus ReceiveTelemetry::mod_finalize()
+{
+  return AS_OK;
+}
 
 } // namespace gramsballoon
