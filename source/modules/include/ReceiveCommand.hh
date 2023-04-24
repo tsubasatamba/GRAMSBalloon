f73a77a5e48ea640ab0300579f26ce8334344385
Tsubasa Tamba <tamba@juno.phys.s.u-tokyo.ac.jp>
2023-04-24 12:59:50 +0900

    merged and fixed hotfix/serial

--- source/modules/include/ReceiveCommand.hh
+++ source/modules/include/ReceiveCommand.hh
@@ -64,7 +64,7 @@ private:
   std::shared_ptr<SerialCommunication> sc_ = nullptr;
   speed_t baudrate_;
   std::string serialPath_;
-  char openMode_ = O_RDWR;
+  mode_t openMode_ = O_RDWR;
   bool startReading_ = false;
   
 };
