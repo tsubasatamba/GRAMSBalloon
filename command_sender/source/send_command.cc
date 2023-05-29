ea53eb8428557fca7049392d5463be439813e25c
grams <grams@astraea.local>
2023-05-29 20:30:45 +0900

    bug fix in command_sender

--- command_sender/source/send_command.cc
+++ command_sender/source/send_command.cc
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
   }
   
   gramsballoon::CommandSender sender;
-  sender.set_serial_port("/dev/tty.usbserial-144130");
+  sender.set_serial_port("/dev/tty.usbserial-1460");
   if ( !sender.open_serial_port() ) {
     std::cout << "Serial port open error -> exit" << std::endl;
     return -1;
