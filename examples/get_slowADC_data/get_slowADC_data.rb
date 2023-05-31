e9757f9c7a3dc5d2673a94cd080a0e06f76a04e1
tamba@raspi5 <tamba@raspi5>
2023-05-31 22:09:42 +0900

    commit on raspi

--- examples/get_slowADC_data/get_slowADC_data.rb
+++ examples/get_slowADC_data/get_slowADC_data.rb
@@ -9,7 +9,7 @@ class MyApp < ANL::ANLApp
     with_parameters(spi_flags: (1<<5) + (1<<6) + (1<<7) + 3, baudrate: MHz*1)
     chain GRAMSBalloon::GetSlowADCData
     with_parameters(chip_select: 17, SPI_manager_name: "SPIManager_slowADC", Va: 5.026,
-                    channels: [0,1,2], num_trials: 2)
+                    channels: [0,1,2], num_trials: 100, chatter: 1)
   end
 end
 
