5ff1f1a1e48c60b419c09e7ce315e334eb3dfcf2
tamba@raspi5 <tamba@raspi5>
2023-05-31 22:27:31 +0900

    raspi settings

--- starter/starter.sh
+++ starter/starter.sh
@@ -2,6 +2,6 @@
 export LD_LIBRARY_PATH=/home/grams/lib:${LD_LIBRARY_PATH}
 export RUBYLIB=/home/grams/lib/ruby:${RUBYLIB}
 
-cd /home/grams/software/GRAMSBalloon/examples/mt_test4
+cd /home/grams/software/GRAMSBalloon/operation/isas_long_run_1
 id=`date "+%Y%m%d%H%M%S"`
-./mt_test4.rb  2>&1 | tee output_${id}.log
+./run.rb  2>&1 | tee output_${id}.log
