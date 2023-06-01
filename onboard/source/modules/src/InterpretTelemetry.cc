df39ba08c0619b88e845daf5824f805d4fba2f54
grams <grams@astraea.local>
2023-06-01 14:38:31 +0900

    tweaks

--- onboard/source/modules/src/InterpretTelemetry.cc
+++ onboard/source/modules/src/InterpretTelemetry.cc
@@ -59,11 +59,12 @@ ANLStatus InterpretTelemetry::mod_analyze()
   telemdef_->interpret();
   currentTelemetryType_ = telemdef_->TelemetryType();
 
-  writeTelemetryToFile(failed);
   if (telemdef_->RunID() != currentRunID_) {
     currentRunID_ = telemdef_->RunID();
     updateRunIDFile();
+    fileIDmp_.clear();
   }
+  writeTelemetryToFile(failed);
   
   return AS_OK;
 }
@@ -94,9 +95,12 @@ void InterpretTelemetry::writeTelemetryToFile(bool failed)
     fileIDmp_[type].second = 0;
   }
 
-  std::ostringstream sout;
-  sout << std::setfill('0') << std::right << std::setw(6) << fileIDmp_[type].first;
-  const std::string id_str = sout.str();
+  std::ostringstream id_sout;
+  id_sout << std::setfill('0') << std::right << std::setw(6) << fileIDmp_[type].first;
+  const std::string id_str = id_sout.str();
+  std::ostringstream run_id_sout;
+  run_id_sout << std::setfill('0') << std::right << std::setw(6) << currentRunID_;
+  const std::string run_id_str = run_id_sout.str();
   const std::string filename = binaryFilenameBase_ + "_" + timeStampStr_ + "_" + type_str + "_" + id_str + ".dat";
   
   if (!failed) {
