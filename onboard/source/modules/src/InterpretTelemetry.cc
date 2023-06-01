e4382a34d801c61bbed209ef04a7161f44c5f1c5
grams <grams@astraea.local>
2023-06-01 15:23:32 +0900

    monitor

--- onboard/source/modules/src/InterpretTelemetry.cc
+++ onboard/source/modules/src/InterpretTelemetry.cc
@@ -19,6 +19,7 @@ ANLStatus InterpretTelemetry::mod_define()
   define_parameter("save_telemetry", &mod_class::saveTelemetry_);
   define_parameter("num_telem_per_file", &mod_class::numTelemPerFile_);
   define_parameter("run_ID_filename", &mod_class::runIDFilename_);
+  define_parameter("binary_filename_base", &mod_class::binaryFilenameBase_);
   define_parameter("chatter", &mod_class::chatter_);
   
   return AS_OK;
@@ -101,7 +102,7 @@ void InterpretTelemetry::writeTelemetryToFile(bool failed)
   std::ostringstream run_id_sout;
   run_id_sout << std::setfill('0') << std::right << std::setw(6) << currentRunID_;
   const std::string run_id_str = run_id_sout.str();
-  const std::string filename = binaryFilenameBase_ + "_" + timeStampStr_ + "_" + type_str + "_" + id_str + ".dat";
+  const std::string filename = binaryFilenameBase_ + "_" + run_id_str + "_" + timeStampStr_ + "_" + type_str + "_" + id_str + ".dat";
   
   if (!failed) {
     telemdef_->writeFile(filename, app);
--- operation/monitor/monitor.rb
+++ operation/monitor/monitor.rb
@@ -12,7 +12,7 @@ class MyApp < ANL::ANLApp
     chain GRAMSBalloon::ReceiveTelemetry
     with_parameters(serial_path: "/dev/tty.usbserial-1430", open_mode: 2, chatter: 1)
     chain GRAMSBalloon::InterpretTelemetry
-    with_parameters(save_telemetry: true, num_telem_per_file: 1000, chatter: 1)
+    with_parameters(save_telemetry: true, num_telem_per_file: 1000, chatter: 1, binary_filename_base: "/Users/grams/data/telemetry/telemetry")
     chain GRAMSBalloon::PushToMongoDB
   end
   attr_accessor :host_name, :database_name
