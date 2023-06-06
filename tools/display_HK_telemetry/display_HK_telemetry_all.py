94397251853bf1eb8b5fa35ef502147418e5deef
grams <grams@astraea.local>
2023-06-06 18:32:58 +0900

    mongodb

---
+++ tools/display_HK_telemetry/display_HK_telemetry_all.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+from display_HK_telemetry import *
+import sys
+import os
+import glob
+
+def get_filename_id(filename):
+    id_str = filename.split("_")[-1].replace(".dat", "")
+    return int(id_str)
+
+def get_filenames(run_id):
+    arr = []
+    dirname = "/Users/grams/data/telemetry/"
+    run_id_str = '{:0=6}'.format(run_id)
+    arr = glob.glob(dirname + "telemetry_" + run_id_str + "_*_HK_*.dat")
+    arr = sorted(arr, key=get_filename_id)
+    print(get_filename_id(arr[0]))
+    return arr
+
+if __name__=="__main__":
+    if len(sys.argv)<3:
+        raise ValueError("USAGE: ./display_HK_telemetry_all.py telemetry_key run_ID_1 run_ID_2 ...")
+    telemetry_key = sys.argv[1]
+    run_ids = [int(i) for i in sys.argv[2:]]
+    filenames = []
+    for run_id in run_ids:
+        arr = get_filenames(run_id)
+        for s in arr:
+            filenames.append(s)
+    print(filenames)
+    run(telemetry_key, filenames)
