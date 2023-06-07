9fff311a22c6af8e3ca1430273e89f29d9b147e8
Tsubasa Tamba <tamba@juno.phys.s.u-tokyo.ac.jp>
2023-06-07 15:11:37 +0900

    Revised command_sender

--- operation/monitor/monitor.rb
+++ operation/monitor/monitor.rb
@@ -10,13 +10,26 @@ class MyApp < ANL::ANLApp
     with_parameters(database: "grams")
     #with_parameters(host: @host_name, database: @database_name, instantiation: false)
     chain GRAMSBalloon::ReceiveTelemetry
-    with_parameters(serial_path: "/dev/tty.usbserial-1430", open_mode: 2, chatter: 1)
+    with_parameters(serial_path: @serial_path, open_mode: 2, chatter: 1)
     chain GRAMSBalloon::InterpretTelemetry
-    with_parameters(save_telemetry: true, num_telem_per_file: 1000, chatter: 0, binary_filename_base: "/Users/grams/data/telemetry/telemetry")
+    with_parameters(save_telemetry: true, num_telem_per_file: 1000, chatter: 0, binary_filename_base: Dir.home + "/data/telemetry/telemetry")
     chain GRAMSBalloon::PushToMongoDB
   end
-  attr_accessor :host_name, :database_name
+  attr_accessor :serial_path
+end
+
+def get_serial_path()
+  filename = Dir.home + "/settings/serial/telemetry_port.txt"
+  ret = ""
+  File.open(filename, mode="rt") do |f|
+    f.each_line do |line|
+      ret = line.strip
+    end
+  end
+  return ret
 end
 
 a = MyApp.new
+a.serial_path = get_serial_path()
+
 a.run(:all, 1)
