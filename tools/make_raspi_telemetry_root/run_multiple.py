4c8165c8035c98c9b82adaad8af6323bdcbaf01f
ShotaArai <shota.arai@phys.s.u-tokyo.ac.jp>
2023-11-26 21:46:48 +0900

    change  make_raspi_telemetry_root

--- tools/make_raspi_telemetry_root/run_multiple.py
+++ tools/make_raspi_telemetry_root/run_multiple.py
@@ -6,16 +6,17 @@ from typing import List
 import os
 import sys
 
+
 def get_filename_id(filename: str):
     id = filename.split("_")[-1].rstrip(".dat")
     print(id)
     return int(id)
 
-def get_filename_time(filename:str):
-    stat = os.stat(filename)
-    t = stat.st_mtime # UNIX time
-    return t
 
+def get_filename_time(filename: str):
+    stat = os.stat(filename)
+    t = stat.st_mtime  # UNIX time
+    return t
 
 
 def run(runIDs: List[int], output_root: str):
@@ -28,21 +29,25 @@ def run(runIDs: List[int], output_root: str):
         for path in globbed_filename:
             t = get_filename_time(path)
             t_now = time.time()
-            if abs(t-t_now) < 5:
+            if abs(t - t_now) < 5:
                 continue
             filenames.append(path)
 
     temp_filelist = "temp_filelist.txt"
-    with open(temp_filelist, "w") as fp :
+    with open(temp_filelist, "w") as fp:
         for filename in filenames:
             fp.write(filename + "\n")
     exe_file = os.environ["HOME"] + "/software/GRAMSBalloon/tools/make_raspi_telemetry_root/build/make_raspi_telemetry_root"
+    print(f"{exe_file} {temp_filelist} {output_root}")
     os.system(f"{exe_file} {temp_filelist} {output_root}")
     os.system(f"rm {temp_filelist}")
 
-if __name__=="__main__":
-    if len(sys.argv)<=2:
-        raise ValueError("USAGE: ./run_multiple.py output_file runI_D1 runID_2 runID_3 ...")
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 2:
+        raise ValueError("USAGE: ./run_multiple.py output_file_header runI_D1 runID_2 runID_3 ...")
     output_file = sys.argv[1]
     run_ids = sys.argv[2:]
-    run(run_ids, output_file)
+    for id in run_ids:
+        output_file_with_id = output_file + f"{id}.root"
+        run([int(id),], output_file_with_id)
