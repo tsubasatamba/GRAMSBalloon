93e2a33b952fd054e1e4e3d3984c7b6b140c896f
ShotaArai <shota.arai@phys.s.u-tokyo.ac.jp>
2025-10-28 00:00:39 +0900

    changed shutdown system example for pGRAMS

--- examples/shutdown_system/shutdown_system.rb
+++ examples/shutdown_system/shutdown_system.rb
@@ -2,12 +2,27 @@
 
 require 'anlnext'
 require 'GRAMSBalloon'
+require 'inifile'
 
 class MyApp < ANL::ANLApp
+    attr_accessor :inifile
     def setup()
+        @inifile = IniFile.load('../../settings/network.cfg')
+        if inifile.nil?
+        puts "Error: network.cfg not found"
+        exit 1
+        end
+        chain GRAMSBalloon::ComMosquittoManager
+        with_parameters(host: ENV["PGRAMS_MOSQUITTO_HOST"], port: ENV["PGRAMS_MOSQUITTO_PORT"].to_i, password: ENV["PGRAMS_MOSQUITTO_PASSWD"], user: ENV["PGRAMS_MOSQUITTO_USER"], keep_alive: 60, chatter: 0, threaded_set: true, device_id: "hubcomputer_c", time_out: 100) do |m|
+        m.set_singleton(0)
+        end
+        chain GRAMSBalloon::ReceiveCommand
+        with_parameters(topic: @inifile["Hub"]["comtopic"], chatter: 100, qos: 0, binary_filename_base: "command_test") do |m|
+        m.set_singleton(0)
+        end
         chain GRAMSBalloon::ShutdownSystem
     end
 end
 
 a = MyApp.new
-a.run(1000, 1)
+a.run(:all, 1)
